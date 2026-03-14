44b0f9a2a047a1f12d18671a7b34b61dc47830e1
txt1931 <rafaelmouoliveira@gmail.com>
2026-03-14 20:15:09 -0300

    Added Java versions toolchain config and stabilize JDK alignment across VS Code/Maven

--- src/test/java/com/example/app/MainTest.java
+++ src/test/java/com/example/app/MainTest.java
@@ -8,16 +8,16 @@ import org.junit.jupiter.api.Test;
 
 class MainTest {
 
-    @Test
-    void mainPrintsHelloWorld() {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        try {
-            System.setOut(new PrintStream(output));
-            Main.main(new String[0]);
-            assertEquals("Hello, World!" + System.lineSeparator(), output.toString());
-        } finally {
-            System.setOut(originalOut);
-        }
+  @Test
+  void mainPrintsHelloWorld() {
+    PrintStream originalOut = System.out;
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    try {
+      System.setOut(new PrintStream(output));
+      Main.main(new String[0]);
+      assertEquals("Hello, World!" + System.lineSeparator(), output.toString());
+    } finally {
+      System.setOut(originalOut);
     }
+  }
 }
