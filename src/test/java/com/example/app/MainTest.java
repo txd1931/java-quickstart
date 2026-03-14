262b7c1dc4acfc97c3d935c91f7903f3468e8f47
txt1931 <rafaelmouoliveira@gmail.com>
2026-03-14 17:24:24 -0300

    Added test class for testing APIs showcase on Main.java

---
+++ src/test/java/com/example/app/MainTest.java
@@ -0,0 +1,23 @@
+package com.example.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void mainPrintsHelloWorld() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        try {
+            System.setOut(new PrintStream(output));
+            Main.main(new String[0]);
+            assertEquals("Hello, World!" + System.lineSeparator(), output.toString());
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
+}
